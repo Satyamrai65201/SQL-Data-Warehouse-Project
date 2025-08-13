8d14eec9df19fd79d39bce6452dad0d9f44bac77
Satyam Rai <72155503+Satyamrai65201@users.noreply.github.com>
2025-08-14 01:23:36 +0530

    erp_cust_az12

--- Scripts/proc_load_silver.sql
+++ Scripts/proc_load_silver.sql
@@ -112,6 +112,24 @@ SELECT  [sls_ord_num]
   FROM [DataWarehouse].[bronze].[crm_sales_details]
 
 
+-----------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
+INSERT INTO [silver].[erp_cust_az12]
+(cid,bdate,gen)
+
+SELECT 
+		CASE WHEN  cid LIKE '%NAS%' THEN  SUBSTRING(cid,4,LEN(cid))
+		ELSE cid
+		END AS cid
+      ,
+	    CASE WHEN bdate> GETDATE()  THEN NULL
+		ELSE bdate 
+		END AS Bdate 
+      ,CASE WHEN UPPER(TRIM(gen))  IN ('M','MALE') THEN 'Male '
+	WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
+	ELSE 'n/a'
+	END AS gen
+  FROM [DataWarehouse].[bronze].[erp_cust_az12]
 
 
 
