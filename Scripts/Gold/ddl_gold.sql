b66117a7b505a1a78bf96493add0474b2cad0a67
Satyam Rai <72155503+Satyamrai65201@users.noreply.github.com>
2025-08-18 00:28:40 +0530

    fact_sales

--- Scripts/Gold/ddl_gold.sql
+++ Scripts/Gold/ddl_gold.sql
@@ -38,6 +38,22 @@ SELECT
   left join silver.erp_px_cat_g1v2  e on  c.cat_id =e.id
   where prd_end_dt is NULL------ filter out all historical  data 
 
- 
+ --===========================================================================================================
+CREATE VIEW gold.fact_sales AS
+SELECT sls_ord_num AS order_number 
+      ,pr.product_key 
+      ,cu.customer_key
+      ,sls_order_dt AS order_date
+      ,sls_ship_dt AS shipping_date
+      ,sls_due_dt AS due_date
+      ,sls_sales AS sales_amount 
+      ,sls_quantity AS quantity
+      ,sls_price AS price
+  FROM silver.crm_sales_details sd
+  LEFT JOIN gold.dim_products pr 
+  ON sd.sls_prd_key= pr.product_number
+  LEFT JOIN gold.dim_customers cu 
+  ON sd.sls_cust_id=cu.customer_id
+
 
 
